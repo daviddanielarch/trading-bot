1a090f8c284062b82ba7d061125bbf75f5bf6f23
David Arch <daviddanielarch@gmail.com>
2025-09-16 12:42:37 -0300

    configure logging

--- liftoff/settings.py
+++ liftoff/settings.py
@@ -143,6 +143,43 @@ ADMIN_SITE_HEADER = "Trading Bot"
 ADMIN_SITE_TITLE = "Trading Bot"
 ADMIN_INDEX_TITLE = "Welcome!"
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'webhooks': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
+
 try:
     from .local import *
 except ModuleNotFoundError:
